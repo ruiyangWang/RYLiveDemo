e3d45a3a5424dc4c1abe135993760140cf6f8d99
ios <ios@iosdeMac-mini.local>
2017-04-25 17:10:00 +0800

    Initial Commit

---
+++ RYLiveDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RYLiveDemo
+//
+//  Created by ios on 2017/4/25.
+//  Copyright © 2017年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RYLiveDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RYLiveDemo
+//
+//  Created by ios on 2017/4/25.
+//  Copyright © 2017年 ios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
